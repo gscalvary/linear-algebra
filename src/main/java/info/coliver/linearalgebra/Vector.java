8a8d2f7a015da564b59549c70c6cf24dc2bc1a0f
Christopher Oliver <coliver650@gmail.com>
2017-02-11 19:56:12 -0600

    Swapped integers for doubles in the Vector class. Added several static methods to the Vector class including: - add - subtract - scale - linear combination - dot product - length - axpy

--- src/main/java/info/coliver/linearalgebra/Vector.java
+++ src/main/java/info/coliver/linearalgebra/Vector.java
@@ -1,20 +1,20 @@
 package info.coliver.linearalgebra;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Vector {
 
-    private ArrayList<Integer> components;
+    private List<Double> components;
 
-    Vector (ArrayList<Integer> components) {
+    Vector (List<Double> components) {
         this.components = components;
     }
 
-    public ArrayList<Integer> getComponents() {
+    public List<Double> getComponents() {
         return components;
     }
 
-    public void setComponents(ArrayList<Integer> components) {
+    public void setComponents(List<Double> components) {
         this.components = components;
     }
 
@@ -24,7 +24,7 @@ public class Vector {
 
     public boolean equals(Vector vector) {
 
-        ArrayList<Integer> otherComponents = vector.getComponents();
+        List<Double> otherComponents = vector.getComponents();
         int oSize = otherComponents.size();
 
         if (oSize != this.components.size()) {
@@ -38,5 +38,148 @@ public class Vector {
         }
 
         return true;
+    }
+
+    public static Optional<Vector> add(List<Vector> terms) {
+
+        if (terms == null || terms.isEmpty()) {
+            return Optional.empty();
+        }
+
+        if (terms.size() == 1) {
+            return Optional.of(new Vector(terms.get(0).getComponents()));
+        }
+
+        int size = terms.get(0).getComponents().size();
+
+        List<Double> sums = new ArrayList<>(size);
+
+        for (int i = 0; i < terms.size(); i++) {
+            if (terms.get(i) == null || terms.get(i).getComponents().size() != size) {
+                return Optional.empty();
+            }
+            for (int j = 0; j < terms.get(i).getComponents().size(); j++) {
+                if (sums.size() <= j) {
+                    sums.add(terms.get(i).getComponents().get(j));
+                } else {
+                    double componentValue = sums.get(j);
+                    componentValue += terms.get(i).getComponents().get(j);
+                    sums.set(j, componentValue);
+                }
+            }
+        }
+
+        return Optional.of(new Vector(sums));
+    }
+
+    public static Optional<Vector> subtract(List<Vector> terms) {
+
+        if (terms == null || terms.isEmpty()) {
+            return Optional.empty();
+        }
+
+        if (terms.size() == 1) {
+            return Optional.of(new Vector(terms.get(0).getComponents()));
+        }
+
+        int size = terms.get(0).getComponents().size();
+
+        List<Double> differences = new ArrayList<>(size);
+
+        for (int i = 0; i < terms.size(); i++) {
+            if (terms.get(i) == null || terms.get(i).getComponents().size() != size) {
+                return Optional.empty();
+            }
+            for (int j = 0; j < terms.get(i).getComponents().size(); j++) {
+                if (differences.size() <= j) {
+                    differences.add(terms.get(i).getComponents().get(j));
+                } else {
+                    double componentValue = differences.get(j);
+                    componentValue -= terms.get(i).getComponents().get(j);
+                    differences.set(j, componentValue);
+                }
+            }
+        }
+
+        return Optional.of(new Vector(differences));
+    }
+
+    public static Optional<Vector> scale(Vector vector, double factor) {
+
+        if (vector == null) {
+            return Optional.empty();
+        }
+
+        List<Double> products = new ArrayList<>();
+
+        vector.getComponents().forEach((component) -> products.add(factor * component));
+
+        return Optional.of(new Vector(products));
+    }
+
+    public static Optional<Vector> axpy(double a, Vector x, Vector y) {
+
+        Optional<Vector> ax = Vector.scale(x, a);
+
+        if (!ax.isPresent()) {
+            return ax;
+        }
+
+        List<Vector> terms = new ArrayList<>();
+        terms.add(ax.get());
+        terms.add(y);
+
+        return Vector.add(terms);
+    }
+
+    public static Optional<Vector> linearCombination(List<Double> coefficients, List<Vector> vectors) {
+
+        if (coefficients == null || coefficients.isEmpty() || vectors == null || vectors.isEmpty() || coefficients.size() != vectors.size()) {
+            return Optional.empty();
+        }
+
+        List<Double> initComponents = new ArrayList<>(Arrays.asList(new Double[coefficients.size()]));
+        Collections.fill(initComponents, 0.0);
+        Vector result = new Vector(initComponents);
+
+        for (int i = 0; i < coefficients.size(); i++) {
+            Optional<Vector> term = axpy(coefficients.get(i), vectors.get(i), result);
+            if (!term.isPresent()) {
+                return term;
+            }
+            result.assign(term.get());
+        }
+
+        return Optional.of(result);
+    }
+
+    public static Optional<Double> dotProduct(Vector x, Vector y) {
+
+        if (x == null || y == null || x.getComponents().size() != y.getComponents().size()) {
+            return Optional.empty();
+        }
+
+        Double result = 0.0;
+
+        for (int i = 0; i < x.getComponents().size(); i++) {
+            result += x.getComponents().get(i) * y.getComponents().get(i);
+        }
+
+        return Optional.of(result);
+    }
+
+    public static Optional<Double> length(Vector vector) {
+
+        if (vector == null) {
+            return Optional.empty();
+        }
+
+        Optional<Double> vectorDotProduct = Vector.dotProduct(vector, vector);
+
+        if (!vectorDotProduct.isPresent()) {
+            return vectorDotProduct;
+        }
+
+        return Optional.of(Math.sqrt(vectorDotProduct.get()));
     }
 }
--- src/test/groovy/info.coliver.linearalgebra/VectorSpec.groovy
+++ src/test/groovy/info.coliver.linearalgebra/VectorSpec.groovy
@@ -10,14 +10,9 @@ class VectorSpec extends Specification {
 
     def setup() {
 
-        def xComponent = new Integer(0)
-        def yComponent = new Integer(0)
-        def zComponent = new Integer(1)
-        components = new ArrayList<Integer>()
-        components.add(xComponent)
-        components.add(yComponent)
-        components.add(zComponent)
-
+        double a = 0.0
+        double b = 1.0
+        components = [a,a,b]
         vector = new Vector(components)
     }
 
@@ -31,14 +26,11 @@ class VectorSpec extends Specification {
     }
 
     def 'Vector components may be changed' () {
+
         given: 'new components'
-        def xComponent = new Integer(1)
-        def yComponent = new Integer(0)
-        def zComponent = new Integer(0)
-        def newComponents = new ArrayList<Integer>()
-        newComponents.add(xComponent)
-        newComponents.add(yComponent)
-        newComponents.add(zComponent)
+        double a = 1.0
+        double b = 0.0
+        def newComponents = [a,b,b]
 
         when: 'setting those components'
         vector.setComponents(newComponents)
@@ -48,14 +40,11 @@ class VectorSpec extends Specification {
     }
 
     def 'Vectors may be tested for equality' () {
+
         given: 'a vector with the same components as another'
-        def xComponent = new Integer(0)
-        def yComponent = new Integer(0)
-        def zComponent = new Integer(1)
-        def newComponents = new ArrayList<Integer>()
-        newComponents.add(xComponent)
-        newComponents.add(yComponent)
-        newComponents.add(zComponent)
+        double a = 0.0
+        double b = 1.0
+        def newComponents = [a,a,b]
         def equalVector = new Vector(newComponents)
 
         expect: 'those vectors should be equal.'
@@ -63,16 +52,11 @@ class VectorSpec extends Specification {
     }
 
     def 'Vectors with a different number of components are unequal' () {
+
         given: 'a vector with different components from another'
-        def wComponent = new Integer(0)
-        def xComponent = new Integer(0)
-        def yComponent = new Integer(1)
-        def zComponent = new Integer(0)
-        def newComponents = new ArrayList<Integer>()
-        newComponents.add(wComponent)
-        newComponents.add(xComponent)
-        newComponents.add(yComponent)
-        newComponents.add(zComponent)
+        double a = 0.0
+        double b = 1.0
+        def newComponents = [a,a,b,a]
         def unequalVector = new Vector(newComponents)
 
         expect: 'those vectors should not be equal.'
@@ -80,14 +64,11 @@ class VectorSpec extends Specification {
     }
 
     def 'Vectors with components of different values are unequal' () {
+
         given: 'a vector with different components from another'
-        def xComponent = new Integer(0)
-        def yComponent = new Integer(1)
-        def zComponent = new Integer(0)
-        def newComponents = new ArrayList<Integer>()
-        newComponents.add(xComponent)
-        newComponents.add(yComponent)
-        newComponents.add(zComponent)
+        double a = 0.0
+        double b = 1.0
+        def newComponents = [a,b,a]
         def unequalVector = new Vector(newComponents)
 
         expect: 'those vectors should not be equal.'
@@ -95,16 +76,11 @@ class VectorSpec extends Specification {
     }
 
     def 'Vectors may be assigned to one another' () {
+
         given: 'a new vector'
-        def wComponent = new Integer(0)
-        def xComponent = new Integer(0)
-        def yComponent = new Integer(1)
-        def zComponent = new Integer(0)
-        def newComponents = new ArrayList<Integer>()
-        newComponents.add(wComponent)
-        newComponents.add(xComponent)
-        newComponents.add(yComponent)
-        newComponents.add(zComponent)
+        double a = 0.0
+        double b = 1.0
+        def newComponents = [a,a,b,a]
         def newVector = new Vector(newComponents)
 
         when: 'assigning that new vector to an older vector'
@@ -112,5 +88,267 @@ class VectorSpec extends Specification {
 
         then: 'the two vectors should be equal.'
         assert vector.equals(newVector)
+    }
+
+    def 'When passing a null argument to Vector add' () {
+
+        expect: 'the return of an empty optional'
+        assert Vector.add(null) == Optional.empty()
+    }
+
+    def 'When passing an empty list to Vector add' () {
+
+        expect: 'the return of an empty optional'
+        assert Vector.add(new ArrayList<Vector>()) == Optional.empty()
+    }
+
+    def 'When passing a single vector to Vector add' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components = [a,b]
+        def vector = new Vector(components)
+        def addends = new ArrayList<Vector>()
+        addends.add(vector)
+
+        expect: 'the return of an equivalent vector'
+        def result = Vector.add(addends)
+        assert result.get() == vector
+    }
+
+    def 'When passing vectors of a different size to Vector add' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components1 = [a,b]
+        def vector1 = new Vector(components1)
+        double c = 3.0
+        def components2 = [a,b,c]
+        def vector2 = new Vector(components2)
+        def addends = new ArrayList<Vector>()
+        addends.addAll(vector1, vector2)
+
+        expect: 'the return of an empty optional'
+        assert Vector.add(addends) == Optional.empty()
+    }
+
+    def 'When passing vectors with of the same size to Vector add' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components1 = [a,b]
+        def vector1 = new Vector(components1)
+        def components2 = [a,b]
+        def vector2 = new Vector(components2)
+        def addends = new ArrayList<Vector>()
+        addends.addAll(vector1, vector2)
+        double c = 4.0
+        def answerComponents = [b,c]
+        def answerVector = new Vector(answerComponents)
+
+        expect: 'a vector of the correct magnitude and direction to be returned'
+        assert Vector.add(addends).get() == answerVector
+    }
+
+    def 'When passing a null argument to Vector scale' () {
+
+        expect: 'the return of an empty optional'
+        assert Vector.scale(null, -0.5) == Optional.empty()
+    }
+
+    def 'When scaling a vector' () {
+
+        given:
+        double a = 2.0
+        double b = 3.0
+        def components = [a,b]
+        def vector = new Vector(components)
+        double factor = 4.0
+        double c = 8.0
+        double d = 12.0
+        def answerComponents = [c,d]
+        def answerVector = new Vector(answerComponents)
+
+        expect: 'a vector of the correct magnitude and direction to be returned'
+        assert Vector.scale(vector, factor).get() == answerVector
+    }
+
+    def 'When passing a null argument to Vector subtract' () {
+
+        expect: 'the return of an empty optional'
+        assert Vector.subtract(null) == Optional.empty()
+    }
+
+    def 'When passing an empty list to Vector subtract' () {
+
+        expect: 'the return of an empty optional'
+        assert Vector.subtract(new ArrayList<Vector>()) == Optional.empty()
+    }
+
+    def 'When passing a single vector to Vector subtract' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components = [a,b]
+        def vector = new Vector(components)
+        def terms = new ArrayList<Vector>()
+        terms.add(vector)
+
+        expect: 'the return of an equivalent vector'
+        def result = Vector.subtract(terms)
+        assert result.get() == vector
+    }
+
+    def 'When passing vectors of a different size to Vector subtract' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components1 = [a,b]
+        def vector1 = new Vector(components1)
+        double c = 3.0
+        def components2 = [a,b,c]
+        def vector2 = new Vector(components2)
+        def terms = new ArrayList<Vector>()
+        terms.addAll(vector1, vector2)
+
+        expect: 'the return of an empty optional'
+        assert Vector.subtract(terms) == Optional.empty()
+    }
+
+    def 'When passing vectors of the same size to Vector subtract' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components1 = [a,b]
+        def vector1 = new Vector(components1)
+        def components2 = [a,b]
+        def vector2 = new Vector(components2)
+        def terms = new ArrayList<Vector>()
+        terms.addAll(vector1, vector2)
+        double c = 0.0
+        def answerComponents = [c,c]
+        def answerVector = new Vector(answerComponents)
+
+        expect: 'a vector of the correct magnitude and direction to be returned'
+        assert Vector.subtract(terms).get() == answerVector
+    }
+
+    def 'When passing a null Vector to axpy' () {
+
+        expect: 'an empty Optional to be returned'
+        double f = 2.0
+        assert Vector.axpy(f, null, null) == Optional.empty()
+    }
+
+    def 'When passing vectors of the same size to Vector axpy' () {
+
+        given:
+        double a = 1.0
+        double b = 2.0
+        def components1 = [a,a]
+        def vector1 = new Vector(components1)
+        def components2 = [a,a]
+        def vector2 = new Vector(components2)
+        double c = 3.0
+        def answerComponents = [c,c]
+        def answerVector = new Vector(answerComponents)
+
+        expect: 'a vector of the correct magnitude and direction to be returned'
+        assert Vector.axpy(b, vector1, vector2).get() == answerVector
+    }
+
+    def 'When passing null arguments to Vector linear combination' () {
+
+        expect: 'an empty Optional to be returned'
+        assert Vector.linearCombination(null, null) == Optional.empty()
+    }
+
+    def 'When passing arguments of unequal size to Vector linear combination' () {
+
+        given:
+        double a = 1.0
+        def components = [a,a]
+        def vector = new Vector(components)
+        def addends = new ArrayList<Vector>()
+        addends.add(vector)
+
+        expect: 'an empty Optional to be returned'
+        assert Vector.linearCombination(components, addends) == Optional.empty()
+    }
+
+    def 'When passing arguments of equal size to Vector linear combination' () {
+
+        given:
+        double a = 1.0
+        def components = [a,a]
+        def vector = new Vector(components)
+        def addends = new ArrayList<Vector>()
+        addends.add(vector)
+        addends.add(vector)
+        double b = 2.0
+        def coefficients = [b,b]
+        double c = 4.0
+        def answerComponents = [c,c]
+        def answerVector = new Vector(answerComponents)
+
+        expect: 'a vector of the correct magnitude and direction to be returned'
+        assert Vector.linearCombination(coefficients, addends).get() == answerVector
+    }
+
+    def 'When passing null arguments to Vector dot product' () {
+
+        expect: 'an empty Optional to be returned'
+        assert Vector.dotProduct(null, null) == Optional.empty()
+    }
+
+    def 'When passing vectors of unequal size to Vector dot product' () {
+
+        given:
+        double a = 0.0
+        def xComponents = [a]
+        def x = new Vector(xComponents)
+        def yComponents = [a,a]
+        def y = new Vector(yComponents)
+
+        expect: 'an empty Optional to be returned'
+        assert Vector.dotProduct(x, y) == Optional.empty()
+    }
+
+    def 'When passing vectors of equal size to Vector dot product' () {
+
+        given:
+        double a = 2.0
+        def xComponents = [a,a]
+        def x = new Vector(xComponents)
+        def yComponents = [a,a]
+        def y = new Vector(yComponents)
+        double b = 8.0
+
+        expect: 'the correct scalar to be returned'
+        assert Vector.dotProduct(x, y).get() == b
+    }
+
+    def 'When passing a null argument to Vector dot length' () {
+
+        expect: 'an empty Optional to be returned'
+        assert Vector.length(null) == Optional.empty()
+    }
+
+    def 'When passing a vector Vector length' () {
+
+        given:
+        double a = 2.0
+        def components = [a,a,a,a]
+        def vector = new Vector(components)
+        double b = 4.0
+
+        expect: 'the correct scalar to be returned'
+        assert Vector.length(vector).get() == b
     }
 }
